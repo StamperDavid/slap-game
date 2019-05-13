ed50538ddd75a95ccfc546b8a9511c83d19a535b
StamperDavid <jamesstamper72@gmail.com>
2019-05-13 00:05:48 -0600

    last attempt at 12:05

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let activeEnemy = 0;
 
 let enemy = [{
-  name: "enemy",
+  name: "object of your agression",
   posture: ["confident", "healthy", "wounded", "defeated"],
-  health: 0,
+  health: 100,
   postureIndex: 0,
   tolerance: 25,
   images: ['', '', '', '']
@@ -16,83 +16,107 @@ let enemy = [{
 //Space
 
 function slap() {
-
   enemy[activeEnemy].health-- //decreases health count by 5
-  if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 25) {
-    enemy[activeEnemy].postureIndex++;
+  if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 0) {
+    enemy[activeEnemy].postureIndex - 5;
   }
   drawEnemy()
-}
-function punch() {
-  debugger
-  enemy[activeEnemy].health-- //decreases health count by 10
-  if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 25) {
-    enemy[activeEnemy].postureIndex++;
-  }
-  drawEnemy()
-}
-function kick() {
-  debugger
-  enemy[activeEnemy].health-- //decreases health count by 20
-  if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 25) {
-    enemy[activeEnemy].postureIndex++;
-  }
-  drawEnemy()
-}
-
-//////////////////////////////////////empty space between character//////////////////////////////////////////////////
-}
-/////////////////////////////////////end of character profile/////////////////////////////////////////////
+},
 
 function drawEnemy() {
   var postureindex = enemy[activeEnemy].postureIndex
   document.getElementById("slap").innerText = enemy[activeEnemy].health
   document.getElementById("enemy-image").setAttribute("src", enemy[activeEnemy].images[postureindex])
   document.getElementById("posture").innerText = enemy[activeEnemy].posture[enemy[activeEnemy].postureIndex]
-  if (enemy[activeEnenmy].postureIndex == enemy[activeEnemy].posture.length++) {
+  if (enemy[activeEnemy].postureIndex == enemy[activeEnemy].posture.length++) {
     document.getElementById('slap-button').disabled = true;
 
   }
   else {
     document.getElementById('slap-button').disabled = false;
-  }
-  if (enemy[activeEnenmy].postureIndex == enemy[activeEnemy].posture.length++) {
-    document.getElementById('punch-button').disabled = true;
 
-  }
-  else {
-    document.getElementById('punch-button').disabled = false;
-  }
-  if (enemy[activeEnenmy].postureIndex == enemy[activeEnemy].posture.length++) {
-    document.getElementById('kick-button').disabled = true;
 
-  }
-  else {
-    document.getElementById('kick-button').disabled = false;
-  }
-}
+    function punch() {
+      enemy[activeEnemy].health-- //decreases health count by 10
+      if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 0) {
+        enemy[activeEnemy].postureIndex - 10;
+      }
+      drawEnemy()
+    }
 
-function ignore() {
-  enemy[activeEnemy].postureIndex = 0
-  enemy[activeEnemy].slap = 0
-  drawEnemy()
-}
+    function drawEnemy() {
+      var postureindex = enemy[activeEnemy].postureIndex
+      document.getElementById("slap").innerText = enemy[activeEnemy].health
+      document.getElementById("enemy-image").setAttribute("src", enemy[activeEnemy].images[postureindex])
+      document.getElementById("posture").innerText = enemy[activeEnemy].posture[enemy[activeEnemy].postureIndex]
+      if (enemy[activeEnemy].postureIndex == enemy[activeEnemy].posture.length++) {
+        document.getElementById('slap-button').disabled = true;
 
-function setActiveEnemy(index) {
-  activeEnemy = index
-  drawEnemy()
-}
+      }
+      else {
+        document.getElementById('slap-button').disabled = false;
 
-function drawButtons() {
-  //
-  var template = ''
-  for (let i = 0; i < enemy.length; i++) {
-    let enemy = i;
-    template += `
+        function kick() {
+          enemy[activeEnemy].health-- //decreases health count by 25
+          if (enemy[activeEnemy].health % enemy[activeEnemy].tolerance == 0) {
+            enemy[activeEnemy].postureIndex - 25;
+          }
+          drawEnemy()
+        }
+        //////////////////////////////////////empty space between character//////////////////////////////////////////////////
+
+        /////////////////////////////////////end of character profile/////////////////////////////////////////////
+
+        function drawEnemy() {
+          var postureindex = enemy[activeEnemy].postureIndex
+          document.getElementById("slap").innerText = enemy[activeEnemy].health
+          document.getElementById("enemy-image").setAttribute("src", enemy[activeEnemy].images[postureindex])
+          document.getElementById("posture").innerText = enemy[activeEnemy].posture[enemy[activeEnemy].postureIndex]
+          if (enemy[activeEnemy].postureIndex == enemy[activeEnemy].posture.length++) {
+            document.getElementById('slap-button').disabled = true;
+
+          }
+          else {
+            document.getElementById('slap-button').disabled = false;
+          }
+          if (enemy[activeEnemy].postureIndex == enemy[activeEnemy].posture.length++) {
+            document.getElementById('punch-button').disabled = true;
+
+          }
+          else {
+            document.getElementById('punch-button').disabled = false;
+          }
+          if (enemy[activeEnemy].postureIndex == enemy[activeEnemy].posture.length++) {
+            document.getElementById('kick-button').disabled = true;
+
+          }
+          else {
+            document.getElementById('kick-button').disabled = false;
+          }
+        }
+
+        function reset() {
+          enemy[activeEnemy].postureIndex = 0
+          enemy[activeEnemy].slap = 0
+          drawEnemy()
+        }
+
+        function setActiveEnemy(index) {
+          activeEnemy = index
+          drawEnemy()
+        }
+
+        function drawButtons() {
+          //
+          var template = ''
+          for (let i = 0; i < enemy.length; i++) {
+            let enemy = [i];
+            template += `
         <button class="btn btn-info" onclick="setActiveEnemy(${i})">${enemy.name}</button>
            `
-  }
-  document.getElementById('dog-buttons').innerHTML = template
-}
+          }
+          document.getElementById('enemy-buttons').innerHTML = template
+        }
 
-drawButtons()
+        drawButtons()
+        debugger
