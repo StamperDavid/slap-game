6242e3fb9c97201c86a5c99c70cbce62bb968458
StamperDavid <jamesstamper72@gmail.com>
2019-05-13 16:07:11 -0600

    done

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ let enemy = {
   health: 100,
   postureIndex: 100,
   tolerance: 25,
-  images: ['', '', '', '']
+  images: ['', '', '', ''],
+  items: []
 }
 
+let items = {
+  block: { name: "block", modifier: 2, description: 'deflects most of damage from a strike!' },
+  dodge: { name: "dodge", modifier: 25, description: 'avoid all damage by avoiding strike' },
+  parry: { name: "parry", modifier: 5, description: 'parry to receive only a glancing bloow' }
+}
 //This
 //Is
 //Just
@@ -16,91 +22,82 @@ let enemy = {
 //Space
 
 function slap() {
-  enemy.health-- //decreases health count by 5
+  enemy.health -= 5 //decreases health count by 5
   if (enemy.health % enemy.tolerance == 0) {
-    enemy.postureIndex -= 5;
+    enemy.postureIndex -= 5);
   }
   drawEnemy()
 }
 
-function drawEnemy() {
-  var postureindex = enemy.postureIndex
-  document.getElementById("health").innerText = enemy.health.toString()
-  document.getElementById("enemy-image").setAttribute("src", enemy.images[postureindex])
-  document.getElementById("posture").innerText = enemy.posture[enemy.postureIndex]
-  if (enemy.postureIndex == enemy.posture.length++) {
-    document.getElementById('slap').disabled = true;
-
-  }
-  else {
-    document.getElementById('slap').disabled = false;
-  }
-}
-
-
 function punch() {
-  enemy.health-- //decreases health count by 10
+  enemy.health -= 10 //decreases health count by 10
   if (enemy.health % enemy.tolerance == 0) {
     enemy.postureIndex -= 10;
   }
   drawEnemy()
 }
 
-function drawEnemy() {
-  var postureindex = enemy.postureIndex
-  document.getElementById("slap").innerText = enemy.health.toString()
-  document.getElementById("enemy-image").setAttribute("src", enemy.images[postureindex])
-  document.getElementById("posture").innerText = enemy.posture[enemy.postureIndex]
-  if (enemy.postureIndex == enemy.posture.length++) {
-    document.getElementById('slap-button').disabled = true;
-
-  }
-  else {
-    document.getElementById('slap').disabled = false;
-  }
-}
-
 function kick() {
-  enemy.health-- //decreases health count by 25
+  enemy.health -= 25 - addMods() //decreases health count by 25
   if (enemy.health % enemy.tolerance == 0) {
     enemy.postureIndex -= 25;
   }
   drawEnemy()
 }
 
-/////////////////////////////////////end of character profile/////////////////////////////////////////////
+function block() {
+  enemy.items.push(items.block)
 
+}
+
+function dodge() {
+  enemy.items.push(items.dodge)
+}
+
+
+function parry() {
+  enemy.items.push(items.parry)
+}
+
+/////////////////////////////////////end of character profile//////////////////////////////////////////////////////
+function addMods() {
+  let modTotal = 0
+  for (let i = 0; i < enemy.items.length; i++) {
+    let item = enemy.items[i]
+    modTotal += item.modifier
+  }
+  enemy.items = []
+  return modTotal
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function drawEnemy() {
-  var postureindex = enemy.postureIndex
-  document.getElementById("slap").innerText = enemy.health.toString()
+  let postureindex = enemy.postureIndex
+  document.getElementById("health").innerText = enemy.health.toString()
   document.getElementById("enemy-image").setAttribute("src", enemy.images[postureindex])
   document.getElementById("posture").innerText = enemy.posture[enemy.postureIndex]
-  if (enemy.postureIndex == enemy.posture.length++) {
+  if (enemy.health <= 0) {
     document.getElementById('slap').disabled = true;
-
+    document.getElementById('punch').disabled = true;
+    document.getElementById('kick').disabled = true;
   }
   else {
     document.getElementById('slap').disabled = false;
-  }
-  if (enemy.postureIndex == enemy.posture.length++) {
-    document.getElementById('punch').disabled = true;
-
-  }
-  else {
     document.getElementById('punch').disabled = false;
-  }
-  if (enemy.postureIndex == enemy.posture.length++) {
-    document.getElementById('kick').disabled = true;
-
-  }
-  else {
     document.getElementById('kick').disabled = false;
   }
 }
 
+//function secondWind({
+//  if (enemy.health == 12 || 24 || 36 || 48 || 60){
+//  enemy.health += 10;
+//};
+//})
+
 function reset() {
   enemy.postureIndex = 0
   enemy.slap = 0
+  enemy.items = []
+  enemy.health = 100
   drawEnemy()
 }
 
